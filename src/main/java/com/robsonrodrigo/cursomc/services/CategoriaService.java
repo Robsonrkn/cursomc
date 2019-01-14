90d0204d0968c737223443de155ad5e0eb3c01dd
Robson Rodrigo <robsonrkn@outlook.com>
2019-01-14 20:23:52 -0200

    Ajustes no endpoint /categorias/{id}

--- src/main/java/com/robsonrodrigo/cursomc/services/CategoriaService.java
+++ src/main/java/com/robsonrodrigo/cursomc/services/CategoriaService.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 
 import com.robsonrodrigo.cursomc.domain.Categoria;
 import com.robsonrodrigo.cursomc.repositories.CategoriaRepository;
+import com.robsonrodrigo.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -14,6 +15,9 @@ public class CategoriaService {
 	
 	public Categoria buscar(Integer id) {
 		Categoria obj = repo.findOne(id);
+		if(obj == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado! Id: "+ id + ", Tipo: "+ Categoria.class.getName());
+		}
 		return obj;
 	}
 }
