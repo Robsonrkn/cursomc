e5610f980ad52c92b97df5cafb4d525e951aa094
Robson Rodrigo <robsonrkn@outlook.com>
2019-02-19 15:54:33 -0300

    Validacao sintatica com Bean validation

--- src/main/java/com/robsonrodrigo/cursomc/services/CategoriaService.java
+++ src/main/java/com/robsonrodrigo/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.robsonrodrigo.cursomc.domain.Categoria;
+import com.robsonrodrigo.cursomc.dto.CategoriaDTO;
 import com.robsonrodrigo.cursomc.repositories.CategoriaRepository;
 import com.robsonrodrigo.cursomc.services.exceptions.DataIntegrityException;
 import com.robsonrodrigo.cursomc.services.exceptions.ObjectNotFoundException;
@@ -50,5 +51,9 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage,String direction, String orderBy){
 		PageRequest pageRequest = new PageRequest(page, linesPerPage, Direction.valueOf(direction),orderBy);
 		return repo.findAll(pageRequest);
+	}
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome()); 
+		
 	}
 }
