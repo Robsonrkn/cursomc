3b9c75dfb3020a9f392805a7351020b2569f173b
Robson Rodrigo <robsonrkn@outlook.com>
2019-02-27 16:16:10 -0300

    Validacao customizada: email nao repetido na atualizacao de Cliente

--- src/main/java/com/robsonrodrigo/cursomc/dto/ClienteDTO.java
+++ src/main/java/com/robsonrodrigo/cursomc/dto/ClienteDTO.java
@@ -7,7 +7,8 @@ import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.NotEmpty;
 
 import com.robsonrodrigo.cursomc.domain.Cliente;
-
+import com.robsonrodrigo.cursomc.services.validation.ClienteUpdate;
+@ClienteUpdate
 public class ClienteDTO implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
