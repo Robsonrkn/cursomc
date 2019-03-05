548bbf1cff4e0b1f92fbdf51c17b978a81f541fb
Robson Rodrigo <robsonrkn@outlook.com>
2019-03-05 17:18:31 -0300

    Inserindo Pedido e pequena atualizacao na classe Pagamento

--- src/main/java/com/robsonrodrigo/cursomc/domain/PagamentoComCartao.java
+++ src/main/java/com/robsonrodrigo/cursomc/domain/PagamentoComCartao.java
@@ -2,9 +2,11 @@ package com.robsonrodrigo.cursomc.domain;
 
 import javax.persistence.Entity;
 
+import com.fasterxml.jackson.annotation.JsonTypeName;
 import com.robsonrodrigo.cursomc.domain.enums.EstadoPagamento;
 
 @Entity
+@JsonTypeName("pagamentoComCartao")
 public class PagamentoComCartao extends Pagamento{
 
 	private static final long serialVersionUID = 1L;
