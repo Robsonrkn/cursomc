d83c4d2d44fbfca864484665d12c5e8df7ee3fd1
Robson Rodrigo <robsonrkn@outlook.com>
2019-03-04 21:05:32 -0300

    Subtotal e total do Pedido

--- src/main/java/com/robsonrodrigo/cursomc/domain/ItemPedido.java
+++ src/main/java/com/robsonrodrigo/cursomc/domain/ItemPedido.java
@@ -29,6 +29,9 @@ public class ItemPedido implements Serializable {
 		this.quantidade = quantidade;
 		this.preco = preco;
 	}
+	public double getSubTotal() {
+		return (preco - desconto) * quantidade;
+	}
 	public ItemPedidoPK getId() {
 		return id;
 	}
