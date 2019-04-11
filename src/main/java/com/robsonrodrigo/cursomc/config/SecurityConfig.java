e3a7a187809ac138bc86726f1452808eee932c18
Robson Rodrigo <robsonrkn@outlook.com>
2019-04-10 22:01:30 -0300

    Implementando autenticacao e geracao do token JWT

--- src/main/java/com/robsonrodrigo/cursomc/config/SecurityConfig.java
+++ src/main/java/com/robsonrodrigo/cursomc/config/SecurityConfig.java
@@ -7,20 +7,32 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import com.robsonrodrigo.cursomc.security.JWTAuthenticationFilter;
+import com.robsonrodrigo.cursomc.security.JwtUtil;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	@Autowired
 	private Environment env;
+	
+	@Autowired
+	private UserDetailsService userDetailsService;
+	
+	@Autowired
+	private JwtUtil jwtUtil;
+	
 	private static final String [] PUBLIC_MATCHERS_GET = {
 			"/produtos/**",
 			"/categorias/**",
@@ -40,8 +52,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 			.antMatchers(HttpMethod.GET,PUBLIC_MATCHERS_GET).permitAll()
 			.antMatchers(PUBLIC_MATCHERS).permitAll()
 			.anyRequest().authenticated();
+		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder());
+	}
+	
 	@Bean
 	CorsConfigurationSource corsConfigurationSource() {
 		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
@@ -51,5 +69,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	@Bean
 	public BCryptPasswordEncoder bCryptPasswordEncoder() {
 		return new BCryptPasswordEncoder();
-	} 
+	}
+	
 }
---
+++ src/main/java/com/robsonrodrigo/cursomc/security/JWTAuthenticationFilter.java
@@ -0,0 +1,74 @@
+package com.robsonrodrigo.cursomc.security;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.robsonrodrigo.cursomc.dto.CredenciaisDTO;
+
+public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+	private AuthenticationManager autheticationManager ;
+	private JwtUtil jwtUtil ;
+	
+	public JWTAuthenticationFilter (AuthenticationManager authenticationManager, JwtUtil jwtUtil) {
+		//setAuthenticationFailureHandler(new JWTAuthenticationFailureHandler());
+		this.autheticationManager = authenticationManager;
+		this.jwtUtil = jwtUtil;
+	}
+	@Override
+	public Authentication attemptAuthentication(HttpServletRequest req, HttpServletResponse res)throws AuthenticationException {
+		try {
+			CredenciaisDTO creds = new ObjectMapper()
+					.readValue(req.getInputStream(), CredenciaisDTO.class);
+			
+			UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(creds.getUsuario(),creds.getSenha(), new ArrayList<>());
+			
+			Authentication auth = autheticationManager.authenticate(authToken);
+			return auth;
+		}
+		catch(IOException e) {
+			throw new RuntimeException(e);
+		}
+		
+	}
+	@Override
+	protected void successfulAuthentication(HttpServletRequest req, HttpServletResponse res, FilterChain chain, Authentication auth) throws IOException, ServletException{
+		String username = ((UserSS) auth.getPrincipal()).getUsername();
+		String token = jwtUtil.generateToken(username);
+		res.addHeader("Authorization", "Bearer" + token);
+	}
+	/*
+	 private class JWTAuthenticationFailureHandler implements AuthenticationFailureHandler {
+
+        @Override
+        public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception)
+                throws IOException, ServletException {
+            response.setStatus(401);
+            response.setContentType("application/json"); 
+            response.getWriter().append(json());
+        }
+
+        private String json() {
+            long date = new Date().getTime();
+            return "{\"timestamp\": " + date + ", "
+                + "\"status\": 401, "
+                + "\"error\": \"Não autorizado\", "
+                + "\"message\": \"Email ou senha inválidos\", "
+                + "\"path\": \"/login\"}";
+        }
+    }
+}*/
+
+}
